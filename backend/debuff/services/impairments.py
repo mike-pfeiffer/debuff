d8a086380ce37138d6454efc4e9280ecd1c216a8
Mike Pfeiffer <pfeiffermj@outlook.com>
2021-10-03 14:31:03 -0500

    fixes missing import for error_response

--- backend/debuff/services/impairments.py
+++ backend/debuff/services/impairments.py
@@ -16,43 +16,19 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-import json
-import subprocess
+from debuff.services.parser import error_response
+from debuff.services.shell_commands import tcshow
 
 
-def build_details(cmd_input, cmd_output, error_msg, is_errored):
-    details = {
-        "command_input": cmd_input,
-        "command_output": cmd_output,
-        "error_message": error_msg,
-        "is_errored": is_errored
-    }
-    return details
+def show_interface_impairments(interface: str):
+    iface_impairments = tcshow(interface)
 
+    if iface_impairments["is_errored"]:
+        cmd_input = iface_impairments["command_input"]
+        cmd_output = iface_impairments["command_output"]
+        errors = iface_impairments["error_message"]
+        return error_response(cmd_input, cmd_output, errors)
 
-def error_handling(cmd_input: str):
-    try:
-        return subprocess.check_output(cmd_input, shell=True)
-    except subprocess.CalledProcessError as e:
-        return e
+    payload = iface_impairments["command_output"]
 
-
-def tcshow(interface: str):
-    cmd_input = f"tcshow {interface}"
-    cmd_output = error_handling(cmd_input)
-    error_msg = None
-    is_errored = False
-
-    if isinstance(cmd_output, Exception):
-        error_msg = cmd_output
-        cmd_output = None
-        is_errored = True
-    else:
-        cmd_output = cmd_output.decode()
-        cmd_output = json.loads(cmd_output)
-
-    details = build_details(cmd_input, cmd_output, error_msg, is_errored)
-
-    return details
-
-print(tcshow("enp2s0"))
+    return payload
