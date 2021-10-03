921d509fa33836edad9a89068f2b58520fe79a39
Mike Pfeiffer <pfeiffermj@outlook.com>
2021-10-03 13:59:25 -0500

    lints api

--- backend/debuff/api/routes/api.py
+++ backend/debuff/api/routes/api.py
@@ -1,7 +1,30 @@
+"""
+An installer for the Debuff program.
+Copyright (C) 2021 Mike Pfeiffer, Dustin Rosarius
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 from fastapi import APIRouter
 from debuff.api.routes import interfaces, tcconfig
 
 router = APIRouter()
 
-router.include_router(interfaces.router, tags=["interfaces"], prefix="/interfaces")
-router.include_router(tcconfig.router, tags=["tc_show"], prefix="/tc_show")
+router.include_router(
+    interfaces.router, tags=["interfaces"], prefix="/interfaces"
+)
+
+router.include_router(
+    tcconfig.router, tags=["tc_show"], prefix="/tc_show"
+)
