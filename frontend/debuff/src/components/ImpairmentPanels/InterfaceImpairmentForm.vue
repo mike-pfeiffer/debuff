10e6e46e3ccae2bcc633e3952c82146355022dc6
Mike Pfeiffer <pfeiffermj@outlook.com>
2021-12-30 11:11:21 -0600

    adds v-layout, v-container

--- frontend/debuff/src/components/ImpairmentPanels/InterfaceImpairmentForm.vue
+++ frontend/debuff/src/components/ImpairmentPanels/InterfaceImpairmentForm.vue
@@ -1,69 +1,73 @@
 <template>
-  <v-card
-    class="mx-auto"
-    max-width="344"
-    tile
-    >
-    <v-card-title class="text-h5">
-      Impairments
-    </v-card-title>
-    <v-form
-        ref="form"
-        v-model="valid"
-        lazy-validation
-    >
-      <v-card-text>
+  <v-container class="my-5">
+    <v-layout row wrap>
+      <v-card
+        class="mx-auto"
+        max-width="344"
+        tile
+        >
+        <v-card-title class="text-h5">
+          Impairments
+        </v-card-title>
+        <v-form
+            ref="form"
+            v-model="valid"
+            lazy-validation
+        >
+          <v-card-text>
 
-        <v-select
-            v-model="select"
-            :items="items"
-            :rules="[v => !!v || 'Item is required']"
-            label="Direction"
-            required
-        ></v-select>
+            <v-select
+                v-model="select"
+                :items="items"
+                :rules="[v => !!v || 'Item is required']"
+                label="Direction"
+                required
+            ></v-select>
 
-        <v-text-field
-            v-model="delay"
-            label="Delay"
-            hint="Range Value: > 0 ms"
-            persistent-hint
-        ></v-text-field>
+            <v-text-field
+                v-model="delay"
+                label="Delay"
+                hint="Range Value: > 0 ms"
+                persistent-hint
+            ></v-text-field>
 
-        <v-text-field
-            v-model="jitter"
-            label="Jitter"
-            hint="Range Value: > 0 ms"
-            persistent-hint
-        ></v-text-field>
+            <v-text-field
+                v-model="jitter"
+                label="Jitter"
+                hint="Range Value: > 0 ms"
+                persistent-hint
+            ></v-text-field>
 
-        <v-text-field
-            v-model="loss"
-            label="Loss"
-            hint="Range Value: 100 >= x > 0 %"
-            persistent-hint
-        ></v-text-field>
-        <v-divider class="mt-4"></v-divider>
-        <v-card-actions>
-          <v-btn
-              :disabled="!valid"
-              color="success"
-              class="mr-4"
-              @click="sendPost()"
-          >
-            Submit
-          </v-btn>
-          <v-spacer></v-spacer>
-          <v-btn
-              color="error"
-              class="mr-4"
-              @click="reset"
-          >
-            Reset
-          </v-btn>
-        </v-card-actions>
-      </v-card-text>
-    </v-form>
-  </v-card>
+            <v-text-field
+                v-model="loss"
+                label="Loss"
+                hint="Range Value: 100 >= x > 0 %"
+                persistent-hint
+            ></v-text-field>
+            <v-divider class="mt-4"></v-divider>
+            <v-card-actions>
+              <v-btn
+                  :disabled="!valid"
+                  color="success"
+                  class="mr-4"
+                  @click="sendPost()"
+              >
+                Submit
+              </v-btn>
+              <v-spacer></v-spacer>
+              <v-btn
+                  color="error"
+                  class="mr-4"
+                  @click="reset"
+              >
+                Reset
+              </v-btn>
+            </v-card-actions>
+          </v-card-text>
+        </v-form>
+      </v-card>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
